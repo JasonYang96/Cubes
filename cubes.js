a5a972b2075a46f700918224987c42bee8000094
Jason Yang <JasonYang96@ucla.edu>
2015-04-11 23:46:44 -0700

    created 8 cubes of different colors and rotated them in the x direction in 60 fps

--- cubes.js
+++ cubes.js
@@ -1,21 +1,29 @@
 var gl;
-
 var NumVertices = 38;
 
+//points holds vertices for triangle strip
+//edges holds vertices for edges
+//theta represents theta variable
 var points = [];
-var colors = [];
 var edges  = [];
-
-var xAxis = 0;
-var yAxis = 1;
-var zAxis = 2;
-
-var axis = 0;
 var theta = [ 0, 0, 0 ];
 
+//theta, vertex color, and color index variables
 var thetaLoc, vColorLoc;
-var vColor = 0;
+var colorIndex = 0;
 
+//model-view matrix
+var pMatrix;
+var fovy = 45;
+var aspect;
+var near = 2;
+var far = 200;
+var tMatrix;
+var mvMatrix;
+var Matrix;
+var MatrixLoc;
+
+//vertex vectors
 var vertices = [
     vec3( -0.5, -0.5,  0.5 ),
     vec3( -0.5,  0.5,  0.5 ),
@@ -24,9 +32,10 @@ var vertices = [
     vec3( -0.5, -0.5, -0.5 ),
     vec3( -0.5,  0.5, -0.5 ),
     vec3(  0.5,  0.5, -0.5 ),
-    vec3(  0.5, -0.5, -0.5 )
+    vec3(  0.5, -0.5, -0.5 ),
 ];
 
+//color vectors
 var vertexColors = [
     [ 0.0, 0.0, 0.0, 1.0 ],  // black
     [ 0.0, 0.0, 1.0, 1.0 ],  // blue
@@ -38,43 +47,60 @@ var vertexColors = [
     [ 1.0, 1.0, 1.0, 1.0 ],  // white
 ];
 
+//array of matrices to create 8 cubes
+var cubes = [
+    translate( 10,  10,  10),
+    translate( 10,  10, -10),
+    translate( 10, -10,  10),
+    translate( 10, -10, -10),
+    translate(-10,  10,  10),
+    translate(-10,  10, -10),
+    translate(-10, -10,  10),
+    translate(-10, -10, -10),
+];
+
 window.onload = function init()
 {
 	//initialize canvas and webGL
     var canvas = document.getElementById( "gl-canvas" );
+    aspect = canvas.width/canvas.height;
 	    
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebG isn't available" ); }
 
+    //create a cube
     colorCube();
 
     //configure WebGL
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
     gl.enable(gl.DEPTH_TEST);
-    gl.depthFunc(gl.LEQUAL);
 
     //  Load shaders and initialize attribute buffers
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
+    // get variables from HTML
     vColorLoc = gl.getUniformLocation( program, "vColor" );
+    thetaLoc = gl.getUniformLocation( program, "theta");
+    MatrixLoc = gl.getUniformLocation( program, "Matrix");
 
+    //create and bind buffer for vertices
     var vBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
 
+    //send the vertex buffer to shader
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
-    thetaLoc = gl.getUniformLocation(program, "theta");
-
+    //event listener
     window.onkeypress = function(event) {
         var key = String.fromCharCode(event.keyCode);
         switch(key) {
           case 'c':
-            vColor = (vColor + 1) % vertexColors.length;
+            colorIndex = (colorIndex + 1) % vertexColors.length;
             break;
         }
     };
@@ -84,30 +110,39 @@ window.onload = function init()
 
 function colorCube()
 {   
-    var indices = [ 3, 2, 1, 6, 5, 4, 1, 0, 3, 7, 0, 4, 7, 6, //vertices for faces 
+    var indices = [ 0, 4, 7, 6, 3, 2, 1, 6, 5, 4, 1, 0, 3, 7, //vertices for faces 
                     0, 1, 2, 3, 1, 2, 6, 7, 1, 5, 2, 6, 5, 6, 4, 7, 0, 4, 3, 7, 0, 3, 4, 5]; //vertices for edges
 
     for ( var i = 0; i < indices.length; ++i ) {
         points.push( vertices[indices[i]] );
-        if (i <= 14)
-            colors.push( vertexColors[1]);
-        else
-            colors.push( vertexColors[6] );
     }
 }
 
 function render() {
+    //clear canvas
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
-    theta[axis] += 2.0;
+    
+    //rotate by theta and send to shader
+    theta[1] += 6.0;
     gl.uniform3fv(thetaLoc, theta);
 
-    //set up color of triangles then draw
-    gl.uniform4fv(vColorLoc, vertexColors[vColor]);
-    gl.drawArrays( gl.TRIANGLE_STRIP, 0, 14 );
+    //apply model-view matrix
+    pMatrix = perspective(fovy, aspect, near, far);
+    tMatrix = translate( 0, 0, -50);
+    mvMatrix = mult(pMatrix, tMatrix);
 
-    //set up color of edges and draw
-    gl.uniform4fv(vColorLoc, [1,1,1,1]);
-    gl.drawArrays( gl.LINES, 14, NumVertices-14);
+    for (var i = 0; i < cubes.length; ++i) {
+        Matrix = mult(mvMatrix, cubes[i]);
+        gl.uniformMatrix4fv(MatrixLoc, false, flatten(Matrix));
+
+        //set up color of triangles then draw
+        gl.uniform4fv(vColorLoc, vertexColors[(colorIndex + i) % cubes.length]);
+        gl.drawArrays( gl.TRIANGLE_STRIP, 0, 14 );
+
+        //set up color of edges and draw
+        gl.uniform4fv(vColorLoc, [1,1,1,1]);
+        gl.drawArrays( gl.LINES, 14, 24);
+    }
 
     //call render on browser refresh
     requestAnimFrame( render );
